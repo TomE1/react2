d5092b3f00eab966eb1d0910e4b75cd9c9d096fa
TomE <tommevans210@gmail.com>
2023-01-30 09:06:54 +0700

    header section is almost finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './Header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocial from './HeaderSocials';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello, my name is</h5>
+        <h1>---</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial />
+
+        <div className='me'>
+          <img src={ME} alt='pic of me' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
