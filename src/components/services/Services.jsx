4c24757eb1a8f6d79dc8ad0eaa9feec5c190793c
TomE <tommevans210@gmail.com>
2023-02-04 10:50:25 +0700

    middle of services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,198 @@
 import React from 'react';
 import './Services.css';
+import { AiOutlineCheck } from 'react-icons/ai';
 
 const Services = () => {
-  return <section id='services'>Services</section>;
+  return (
+    <section id='services'>
+      <h5>What's Offered</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX ARTICLE */}
+
+        {/* WEB DEVELOPMENT BEGIN */}
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* CONTENT CREATION BEGIN */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className='service__list-icon' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                vitae?
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* CONTENT CREATION END */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
