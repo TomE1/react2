db6cbd91937fe9fa072d1e0c58eafda2a7f140ae
TomE <tommevans210@gmail.com>
2023-02-05 05:33:51 +0700

    midway through portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import './Portfolio.css';
 
 const Portfolio = () => {
-  return <section id='portfolio'>Portfolio</section>;
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com/TomE1' className='btn'>
+            GitHub
+          </a>
+          <a href='https://github.com/TomE1' className='btn btn-primary'>
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
