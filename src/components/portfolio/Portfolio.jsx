b6cd3eba17f8f3909f08f4ffb1acfa947a3e5ce5
TomE <tommevans210@gmail.com>
2023-02-06 06:59:56 +0700

    finished portfolio styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
 
 const Portfolio = () => {
   return (
@@ -9,15 +15,114 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <div className='portfolio__item-image'></div>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG1} alt='' />
+          </div2>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com/TomE1' className='btn'>
-            GitHub
-          </a>
-          <a href='https://github.com/TomE1' className='btn btn-primary'>
-            Live Demo
-          </a>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
+        <article className='portfolio__item'>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG2} alt='' />
+          </div2>
+          <h3>This is a portfolio item title</h3>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG3} alt='' />
+          </div2>
+          <h3>This is a portfolio item title</h3>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG4} alt='' />
+          </div2>
+          <h3>This is a portfolio item title</h3>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG5} alt='' />
+          </div2>
+          <h3>This is a portfolio item title</h3>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div2 className='portfolio__item-image'>
+            <img src={IMG6} alt='' />
+          </div2>
+          <h3>This is a portfolio item title</h3>
+
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/TomE1' className='btn'>
+              GitHub
+            </a>
+            <a
+              href='https://dribble.com/Alien_pixels'
+              className='btn btn-primary'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+       
       </div>
     </section>
   );
